5e84ce86e716055a9d1fb7db82dac93f988b4e58
qualidadehsr-rgb <qualidade.hsr@gruposanta.com.br>
2026-04-13 10:12:55 -0400

    feat: adiciona stg_internacoes e stg_movimentacoes

---
+++ dbt/pipeline_analytics/models/staging/stg_internacoes.sql
@@ -0,0 +1,22 @@
+-- referencia tabela de internações
+with source as (
+    select * from {{source('raw', 'internacoes')}}
+),
+tipado as (
+    select * except(COD_PACIENTE, ATENDIMENTO, DT_HR_ATENDIMENTO),
+        {{cast_inteiro('COD_PACIENTE')}} as COD_PACIENTE,
+        {{cast_inteiro('ATENDIMENTO')}} as ATENDIMENTO,
+        {{cast_datetime('DT_HR_ATENDIMENTO')}} as DT_HR_ATENDIMENTO
+    from source
+),
+transformado as (
+    select
+        COD_PACIENTE,
+        ATENDIMENTO,
+        ORIGEM_ATEND,
+        DT_HR_ATENDIMENTO,
+        LEITO,
+        UNIDADE
+    from tipado
+)
+select * from transformado
